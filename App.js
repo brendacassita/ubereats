e49ce6ec4aee254cb563b161c2321800cb3e27cb
Brenda <brendacassita@gmail.com>
2022-07-27 19:59:49 -0600

    status bar

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
     <View style={styles.container}>
       <RestaurantDetailsScreen />
       {/* <HomeScreen /> */}
-    <StatusBar style="auto" />
+    <StatusBar style="light" />
     </View>
   );
 }
