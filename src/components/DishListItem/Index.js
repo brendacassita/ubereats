72f728c85ee53a2c0a339547eb543ed95bc02f31
Brenda <brendacassita@gmail.com>
2022-07-28 19:28:54 -0600

    dish list item

---
+++ src/components/DishListItem/Index.js
@@ -0,0 +1,11 @@
+import {View, Text, StyleSheet, Image} from "react-native"
+
+const DishListItem = () => {
+  return (
+    <View>
+      <Text>Title</Text>
+    </View>
+  )
+}
+
+export default DishListItem 
