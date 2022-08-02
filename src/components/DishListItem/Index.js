75ce316b637625af78d3907ea66cbc40aca86817
Brenda <brendacassita@gmail.com>
2022-08-02 15:57:07 -0600

    styles

--- src/components/DishListItem/Index.js
+++ src/components/DishListItem/Index.js
@@ -2,16 +2,18 @@ import {View, Text, StyleSheet, Image} from "react-native"
 
 const DishListItem = ({dish}) => {
   return (
-    <View>
-      <Text>{dish.name}</Text>
-      <Text>{dish.description}</Text>
-      <Text>{dish.price}</Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>{dish.name}</Text>
+      <Text style={styles.description}>{dish.description}</Text>
+      <Text style={styles.price}>{dish.price}</Text>
 
 
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  
+})
 
 export default DishListItem 
