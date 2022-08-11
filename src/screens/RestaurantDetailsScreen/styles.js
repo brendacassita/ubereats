42f8f0cb2ee36c1e847b2a36db8de5fc0255efef
Brenda <brendacassita@gmail.com>
2022-08-10 20:00:05 -0600

    styles

---
+++ src/screens/RestaurantDetailsScreen/styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native"
+
+export default StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  iconContainer: {
+    position: "absolute",
+    top: 40,
+    left: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 5/3,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "600",
+    marginVertical: 10,
+    margin: 10,
+  },
+  subtitle: {
+    color: 'grey',
+    fontSize: 15,
+  },
+  container: {
+    margin: 10,
+  }
+})
+
+
+  
